5211caeebcbf35630081ff98ac87ffaab1888299
mik284 <mikeykamau222@gmail.com>
2024-06-03 22:50:27 +0300

    create rest controllers

--- src/main/java/com/googleOauth2/demo/GoogleOauth2Application.java
+++ src/main/java/com/googleOauth2/demo/GoogleOauth2Application.java
@@ -2,10 +2,24 @@ package com.googleOauth2.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
+@RestController
 public class GoogleOauth2Application {
 
+	@GetMapping
+	public String welcomeMessage() {
+		return "Hello Welcome to Google OAuth 2.0!";
+	}
+	@GetMapping("/user")
+	public Principal user(Principal principal) {
+		System.out.println("username: " +principal.getName());
+		return principal;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GoogleOauth2Application.class, args);
 	}
