292be16776d632ee297d29508ffbd9f9a5c97488
mik284 <mikeykamau222@gmail.com>
2024-06-03 22:12:58 +0300

    initial commit

---
+++ src/main/java/com/googleOauth2/demo/GoogleOauth2Application.java
@@ -0,0 +1,13 @@
+package com.googleOauth2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/googleOauth2/demo/GoogleOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googleOauth2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
