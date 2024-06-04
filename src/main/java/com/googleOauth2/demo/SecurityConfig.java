738094588ae837f18e7054089b6930d407efdb91
mik284 <mikeykamau222@gmail.com>
2024-06-04 14:50:50 +0300

    add security configurations to handle auth req

--- src/main/java/com/googleOauth2/demo/SecurityConfig.java
+++ src/main/java/com/googleOauth2/demo/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(authorizeRequests ->
+                .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/oauth2/**", "/login/**", "/user").permitAll()
                                 .anyRequest().authenticated()
